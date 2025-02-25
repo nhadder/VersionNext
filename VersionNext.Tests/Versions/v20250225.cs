9bbd548fb9783936acef2461b099904f3872e440
nhadder <nhadder@revealdata.com>
2025-02-25 14:26:24 -0700

    reorganizing solution structure

---
+++ VersionNext.Tests/Versions/v20250225.cs
@@ -0,0 +1,13 @@
+﻿using VersionNext.Models;
+
+namespace VersionNext.Tests.Versions
+{
+    public class v20250225 : DatabaseVersion
+    {
+        public v20250225() : base(2025, 2, 25)
+        {
+            AddSqlUpdate("CREATE TABLE [dbo].[TestTable] ([Id] INT PRIMARY KEY);");
+            AddSqlUpdate("INSERT INTO [dbo].[TestTable] ([Id]) VALUES (1);");
+        }
+    }
+}
