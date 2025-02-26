8645a71e19aa92bc0d871df9c5dfd7322c8b7ab4
nhadder <nhadder@revealdata.com>
2025-02-25 17:38:52 -0700

    better packing

--- Pack.ps1
+++ Pack.ps1
@@ -1,1 +1,2 @@
-C:\Nuget\nuget.exe pack VersionNext\VersionNext.nuspec
+dotnet test VersionNext.sln --configuration Release
+C:\Nuget\nuget.exe pack VersionNext\VersionNext.nuspec -properties Configuration=Release -symbols -OutputDirectory Package\
